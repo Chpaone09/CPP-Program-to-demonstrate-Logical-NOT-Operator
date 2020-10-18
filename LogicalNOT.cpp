b1a13a2d1b0c7090ec00be0ea93c71e7416f07a5
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-19 03:26:51 +0530

    Program to demonstrate Logical NOT ( ! ) Operator in cpp

---
+++ LogicalNOT.cpp
@@ -0,0 +1,22 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate Logical NOT ( ! ) Operator in cpp.
+Author  : Chpaone09®
+Date    : Oct 19, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+        bool a = true;
+
+        cout<<"\n\n\t Original Value of A >> "<< a;
+
+        cout<<"\n\n\t After applying Logical NOT Operator on A >> "<< !a;
+
+    
+}
